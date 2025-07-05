bec505e2f679de12ec2b1ba8cc8ebc107b75afa3
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-07-05 11:29:32 +0530

    done validation in modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/Notestate";
 
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           {/* Default route or fallback */}
           <Route path="*" element={<Home />} />
         </Routes>
