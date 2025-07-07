813e2c2191f8f3a79e1389e80f0973a8974a3b7c
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-07-07 20:44:55 +0530

    alert display made

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -7,21 +8,34 @@ import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NoteState from "./context/notes/Notestate";
 import Authstate from "./context/authentication/Authstate";
-
+import Alert from "./components/Alert";
 function App() {
+ const [alert, setAlert] = useState(null);
+  const showAlert = (msg, type) => {
+    setAlert({
+      message: msg,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
+
   return (
     <Authstate>
     <NoteState>
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar  showalert={showAlert}/>
+        <Alert alert={alert}></Alert>
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/about"  element={<About  showalert={showAlert}/>} />
+          <Route path="/home" element={<Home  showalert={showAlert} />} />
+          <Route path="/login" element={<Login  showalert={showAlert} />} />
+          <Route path="/signup" element={<Signup  showalert={showAlert} />} />
           {/* Default route or fallback */}
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<Home  showalert={showAlert} />} />
         </Routes>
       </Router>
     </div>
