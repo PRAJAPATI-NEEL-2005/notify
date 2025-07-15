11bde725ebd30fe5c722344e1ae2cb131a5a1c41
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-07-15 10:28:14 +0530

    used env var as uri

--- Backend/db.js
+++ Backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/notify", {
+    await mongoose.connect(process.env.MONGO_URI, {
       
     });
     console.log("MongoDB connected successfully");
